fb1aef48d0d9dad10de3596b20c537db41615e30
Godfrey Huang <jeanimator@gmail.com>
2018-09-20 15:20:14 +0800

    Update plugin.h

--- src/plugin.h
+++ src/plugin.h
@@ -4,9 +4,8 @@
 #define	USEPROGRESSWIN	1			// Set to 1 to use progress window, which seems buggy so defaults to off.
 
 //
-#define ID_BLOCKSTART			0x00006680
+#define ID_BLOCKSTART			0x00008711
 
 // Previous entries used by other plugins I wrote...DO NOT CHANGE THIS OR USE OTHERS it has been
 //		provided to me by Alias and I will be using other following values. -comet
-#define ID_EYEDEFORMER			ID_BLOCKSTART+0		// 0x00006675(holy)
-#define ID_MESHOPNODE   		ID_BLOCKSTART+1     // 0x00006676(holy)
+#define ID_PLUGINTEMPLATE			ID_BLOCKSTART+0
